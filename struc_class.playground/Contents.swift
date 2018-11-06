6df234fdda37505e048e5ebeae68af322a79bd57
Mohamed zakaria <programmer.Mohamed.Zakaria333@gmail.com>
2018-11-07 00:02:24 +0200

    adding a comment

---
+++ struc_class.playground/Contents.swift
@@ -0,0 +1,102 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//i learn how to clone projects
+//Initializers (swift 4)
+//=================
+//init() {}
+// perform some initialization here
+//must be in class or structure
+//=================
+//structure(swift 4)
+//==============================================================================================
+struct SomeStructure {
+    // structure definition goes here
+}
+//==
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+var f = Fahrenheit()
+print("The default temperature is \(f.temperature)° Fahrenheit")
+//==
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+// boilingPointOfWater.temperatureInCelsius is 100.0
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+// freezingPointOfWater.temperatureInCelsius is 0.0
+//==
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+    }
+    init(white: Double) {
+        red   = white
+        green = white
+        blue  = white
+    }
+}
+let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+let halfGray = Color(white: 0.5)
+//let veryGreen = Color(0.0, 1.0, 0.0)
+// this reports a compile-time error - argument labels are required
+
+
+
+//==
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+//خلى بالك هنا وهو انى عملت 
+let resobject = Resolution(width: 78,height: 788)
+
+//======================================================================================================
+//classes  (swift4)
+//==================
+class SomeClass {
+    
+}
+//===
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+//==
+//==================
+//objects (swift 4)
+//==================
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+//==================
+
+
+
+
+
+
+
+
